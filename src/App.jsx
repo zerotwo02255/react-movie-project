30f73de443f51ed183ea72b49fead3dbebf0e9fc
zerotwo02255 <ankuwizard5@gmail.com>
2026-07-03 15:06:49 +0545

    sdfs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
-import React from "react";
-import Search from "./Search";
+import React, { useEffect,useState } from "react";
+import Search from "./components/Search";
+
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
 
-  const [searchTerm,setSearchTerm] = useState("");
+  const [searchTerm,setSearchTerm] = useState('');
+
+  const[error,setError] = useState(null);
+
+  
+    const fetchMovies = async () => {
+      try{
+        const endpoint =`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+        const response = await fetch(endpoint,API_OPTIONS);
+
+        if(!response.ok){
+          throw new Error("Failed to fetch movies");
+        }
+
+        const data=await response.json();
+        console.log(data);
+      } 
+      
+      catch (error) {
+        console.error("Error fetching movies:" + error);
+        setError("Failed to fetch movies. Please try again later.");
+      }
+    }
+
+   useEffect(() => {
+    fetchMovies();
+
+
+  },[]);
+
   return (
     <main>
       <div className="pattern" />
@@ -16,11 +57,19 @@ const App = () => {
             </span>
               that youll enjoy without any time waste  
            
-            <span className="fancy-text"> ACTION HORROR </span>
+            <span className="text-gradient"> ACTION HORROR </span>
           </h1>
         </header>
 
         < Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+        <section className="all-movies">
+           <h2>All Movies</h2>
+           {error && <p className="text-red-500">{error}</p>}
+
+        </section>
+
+
       </div>
     </main>
   );
---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Search = ({searchTerm,setSearchTerm}) => {
+  return (
+    <div className="search">
+        <div>
+            <img src="./search.svg" alt="search" />
+
+            <input type="text" 
+            placeholder="Search for a movie..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Search;
